beac2c5d001df3a3896cde86ddf75b545c96b22e
Sudish Shrestha <sudishrestha@gmail.com>
2020-10-15 08:56:18 +0545

    Stock production lot bug removed

--- odooCustomization/models/stock_production_lot.py
+++ odooCustomization/models/stock_production_lot.py
@@ -1,18 +1,16 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-
 from odoo import api, fields, models, _
 from odoo.exceptions import UserError
-
-
 class ProductionLot(models.Model):
     _inherit = 'stock.production.lot'
-
-
     @api.multi
     def name_get(self):
         result = []
         for record in self:
-            record_name = record.name + '[' + str(record.quant_ids[(len(record.quant_ids)-1)].qty)+ ']'
+            try:
+                record_name = record.name + '[' + str(record.quant_ids[(len(record.quant_ids)-1)].qty)+ ']'
+            except:
+                record_name = record.name + '[' + ']'
             result.append((record.id, record_name))
         return result
